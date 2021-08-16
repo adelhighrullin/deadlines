1aa07f68eb74ed3e9e074bc2c14b43881f12fed3
adelhighrullin <adelhighrullin@gmail.com>
2021-08-16 20:46:07 +0300

    added api via rest framework

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TaskSerializer
 from .models import Task
 
-
+class TaskView(viewsets.ModelViewSet):
+  serializer_class = TaskSerializer
+  queryset = Task.objects.all()
+  
 def index(request):
   return HttpResponse("Hello")
 
