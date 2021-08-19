8aeb0e928ed16a1310899e3ad12c26d4f278f052
adelhighrullin <adelhighrullin@gmail.com>
2021-08-19 12:57:24 +0300

    added edit modal window

--- frontend/src/TaskList.js
+++ frontend/src/TaskList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Modal from 'react-modal';
 
 export default class TaskList extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      openModal: false,
       tasks: [],
     };
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
   }
 
   componentDidMount() {
@@ -21,13 +25,53 @@ export default class TaskList extends Component {
       .catch((err) => console.log(err));
   };
 
-  renderTasks = () => {
-    const newTasks = this.state.tasks;
+  handleCloseModal = () => {
+    this.setState({ openModal: false });
+  }
 
-    return newTasks.map((task) => (
+  handleOpenModal = () => {
+    this.setState({ openModal: true });
+  }
+
+  deleteTask = (task) => {
+    this.setState({ openModal: !this.state.openModal })
+  };
+
+  editTask = (task) => {
+    this.handleOpenModal();
+    console.log("edit clicked");
+    console.log(this.state.openModal);
+    /*
+    return (
+      <div>
+        <Modal
+          isOpen={this.state.openModal}
+        >
+          <p>modal window hehehehlksdjfkjafh</p>
+          <p>this is clicked task name: {task.name}</p>
+          <button onClick={this.handleCloseModal}>close ths windwo</button>
+        </Modal>
+      </div>
+    );
+    */
+  };
+
+  renderTasks = () => {
+    const getTasks = this.state.tasks;
+
+    return getTasks.map((task) => (
       <div>
         <p>{task.name}</p>
         <p>{task.description}</p>
+        <button onClick={() => this.editTask(task)}>Edit</button>
+        <Modal
+          isOpen={this.state.openModal}
+        >
+          <p>modal window hehehehlksdjfkjafh</p>
+          <p>this is clicked task name: {task.name}</p>
+          <button onClick={this.handleCloseModal}>close ths windwo</button>
+        </Modal>
+        <button onClick={() => this.deleteTask(task)}>Delete</button>
       </div>
     ));
   }
@@ -37,6 +81,13 @@ export default class TaskList extends Component {
       <div className="TaskList">
         <p>Task List</p>
         {this.renderTasks()}
+        <button onClick={this.handleOpenModal}>faihroieuqhibf</button>
+        <Modal
+          isOpen={this.state.openModal}
+        >
+          <p>slkdjfsldfkjsdf test test etst</p>
+          <button onClick={this.handleCloseModal}></button>
+        </Modal>
       </div>
     );
   }
